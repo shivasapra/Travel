55a86003a9351d6623f7fb436c0ec212cd6b5a5a
shiva <ashishsapra7@gmail.com>
2019-02-06 00:52:21 +0530

    Invoice T&C

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -1,5 +1,6 @@
 <p>Hi,</p>
 
-<p>Someone has invited you to access their account.</p>
+<p>To activate your account click below.</p>
+<p>Employee can not open buisness within ten miles.</p>
 
 <a href="{{ route('accept', $token) }}">Click here</a> to activate!
--- resources/views/invoice-print.blade.php
+++ resources/views/invoice-print.blade.php
@@ -179,8 +179,41 @@ Invoice
           </div>
         </div>
         <!-- /.col -->
-      </div>
-      <!-- /.row -->
+      </div><hr>
+      <p class="text-muted" style="margin-top: 10px;">
+        <strong>*T&C</strong><br>
+        Foreign & Commonwealth Office Travel Advice: The Foreign & Commonwealth Office (FCO) issues travel advice on destinations, which includes information on passports, visas, health, safety and security and more. For more information refer to the link: <a href=" https://www.gov.uk/foreign-travel-advice"> https://www.gov.uk/foreign-travel-advice</a> New Security Requirements For Airlines : Phones, laptops and tablets larger than 16.0cm x 9.3cm x 1.5cm not allowed in the cabin on flights to the UK from Turkey, Lebanon, Egypt, Saudi Arabia, Jordan and Tunisia. For more information please see <a href="https://www.gov.uk/government/news/additional-hand-luggage-restrictions-on-some-flights-to-the-uk">https://www.gov.uk/government/news/additional-hand-luggage-restrictions-on-some-flights-to-the-uk</a>
+      </p>
+      <p class="text-muted" style="margin-top: 10px;">
+        NOTES :
+        Reconfirmation of any onward / return journey is passengers responsibility.
+        Valid travel documentation such as valid passport, visa, health precautions are passengers responsibility.
+        Timings are subject to change, please reconfirm with your airline operator before you fly.
+        Any reissue / revalidation / cancellation will incur a fee.
+        Any passengers under 18 years on age travelling to South Africa will be denied boarding if not carrying their original birth certificate.
+        Any passengers who hold an OCI who travel to INDIA without their original OCI card will be denied boarding.
+        Passengers travelling to/ or via USA/CANADA : will require an ESTA at least 72 hours prior to travel, even for transit purposes. Children under 18 travelling to South Africa and Botswana : All minors travelling will be required to carry certified copies Birth Certificate, and in the event that only one parent is travelling, certified written consent from the other parent to allow the child to travel.
+        If there are any long (or overnight transits) , include multiple transit points enroute within a country, it is the passengers responsibility to make the necessary accommodation and visa arrangements.
+        PASSENGER NOTICE :
+        Carriage and other services provided by the carrier are subject to conditions of contract, which are hereby incorporated by reference. These conditions may be obtained from the issuing carrier. The itinerary/receipt constitutes the passenger ticket for the purposes of Article 3 of the Warsaw convention, except where the carrier delivers to the passenger another document complying with the requirements of Article 3. If the passenger's journey involves an ultimate destination or stop in a country other than the country of departure the Warsaw Convention may be applicable, and the convention governs, and in most cases limits, the liability of carriers for death or personal injury and in respect of loss of or damage to baggage. See also notices headed Advice to International Passengers on limitation of liability and notice of baggage liability limitations. Full conditions can be found at
+        <a href="WWW.IATA.ORG">WWW.IATA.ORG</a>
+        or by clicking 
+        HERE
+      </p>
+      <p class="text-muted" style="margin-top: 10px;">
+        If you are travelling to USA, all qualified Visa Waiver Program travellers will be required to obtain electronic travel authorization prior to boarding an air or sea carrier to the United States.
+        Electronic System for Travel Authorization (ESTA) to USA
+        Travellers who do not receive travel authorization prior to their departure may be denied boarding, experience delays or be denied admission into the United
+        States. Applications may be submitted at anytime prior to travel, but no less than 72 hours prior to departure.
+        Travel Authorization is obtained through an online registration system known as the Electronic System for Travel Authorization (ESTA). If your registration
+        is successful, it will be valid for multiple applications for two years or until the date on which your passport expires, which ever comes first.
+        Submit your ESTA Application at 
+        <a href="WWW.IATA.ORG">WWW.IATA.ORG</a>
+      </p>
+      <p class="text-muted" style="margin-top: 10px;">
+        Note : There is no liability if airline(s) above cease to trade, unless Scheduled Airline Failure Insurance (SAFI) has been paid.
+        Yours Sincerely
+      </p>
 
       <!-- this row will not appear when printing -->
       {{-- <div class="row no-print">
