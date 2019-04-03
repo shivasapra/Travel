6e1237780ad77bd0211628da726d168ca661b2f4
shiva <ashishsapra7@gmail.com>
2019-04-03 18:08:18 +0530

    Invoice Changes

--- database/migrations/2019_01_27_114909_create_invoice_infos_table.php
+++ database/migrations/2019_01_27_114909_create_invoice_infos_table.php
@@ -18,6 +18,7 @@ class CreateInvoiceInfosTable extends Migration
             $table->integer('invoice_id');
             
             $table->string('service_name')->nullable();
+
             $table->string('airline_name')->nullable();
             $table->string('source')->nullable();
             $table->string('destination')->nullable();
@@ -25,12 +26,29 @@ class CreateInvoiceInfosTable extends Migration
             $table->string('adult')->nullable();
             $table->string('child')->nullable();
             $table->string('infant')->nullable();
-            $table->Date('infant_dob')->nullable();
+            $table->string('infant_dob')->nullable();
+
             $table->string('name_of_visa_applicant')->nullable();
             $table->string('passport_origin')->nullable();
             $table->string('visa_country')->nullable();
             $table->string('visa_type')->nullable();
             $table->string('visa_charges')->nullable();
+
+            $table->string('hotel_city')->nullable();
+            $table->string('hotel_country')->nullable();
+            $table->string('hotel_name')->nullable();
+            $table->string('check_in_date')->nullable();
+            $table->string('check_out_date')->nullable();
+            $table->string('no_of_children')->nullable();
+            $table->string('no_of_rooms')->nullable();
+
+            $table->string('name_of_insurance_applicant')->nullable();
+
+            $table->string('insurance_remarks')->nullable();
+            $table->string('local_sight_sceen_remarks')->nullable();
+            $table->string('other_facilities_remarks')->nullable();
+            $table->string('car_rental_remarks')->nullable();
+            $table->string('local_transport_remarks')->nullable();
 
             $table->integer('quantity')->nullable();
             $table->string('currency')->nullable();
