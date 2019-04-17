30a626964be1256137a8ef0750478106a5148b5b
shiva <ashishsapra7@gmail.com>
2019-04-18 00:41:29 +0530

    member_passport_place field added in clientFamily model

--- database/migrations/2019_04_10_171044_create_client_families_table.php
+++ database/migrations/2019_04_10_171044_create_client_families_table.php
@@ -19,6 +19,7 @@ class CreateClientFamiliesTable extends Migration
             $table->string('member_name')->nullable();
             $table->string('member_DOB')->nullable();
             $table->string('member_passport_no')->nullable();
+            $table->string('member_passport_place')->nullable();
             $table->string('member_passport_front')->nullable();
             $table->string('member_passport_back')->nullable();
             $table->timestamps();
