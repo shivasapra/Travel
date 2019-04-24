02ac4761cda248013782c4791f3578bf3e7b23c2
shiva <ashishsapra7@gmail.com>
2019-04-24 14:31:18 +0530

    DOM pdf fixess

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -8,6 +8,7 @@
 <html>
 <head>
   <meta charset="utf-8">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>@yield('title')</title>
