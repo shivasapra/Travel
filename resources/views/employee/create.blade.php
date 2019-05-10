f201646d9f193b285e2c23d24a70366ad51d7e77
shiva <ashishsapra7@gmail.com>
2019-05-10 21:10:36 +0530

    Old Values in Employee form

--- resources/views/employee/create.blade.php
+++ resources/views/employee/create.blade.php
@@ -47,19 +47,19 @@ Employee Registration
 					<div class="col-md-4">
 					<div class="form-group">
 						<label for="first_name">First Name</label>
-						<input type="text" name='first_name' required class="form-control">
+						<input type="text" name='first_name' value="{{old('first_name')}}" required class="form-control">
 					</div>
 					</div>
 					<div class="col-md-4">
 					<div class="form-group">
 						<label for="middle_name">Middle Name</label>
-						<input type="text" name='middle_name' class="form-control">
+						<input type="text" name='middle_name' value="{{old('middle_name')}}" class="form-control">
 					</div>
 					</div>
 					<div class="col-md-4">
 					<div class="form-group">
 						<label for="last_name">Last Name</label>
-						<input type="text" name='last_name' required class="form-control">
+						<input type="text" name='last_name' value="{{old('last_name')}}" required class="form-control">
 					</div>
 					</div>
 					</div>
@@ -67,13 +67,13 @@ Employee Registration
 					<div class="col-md-6">
 					<div class="form-group">
 						<label for="father_name">Father's Name</label>
-						<input type="text" name='father_name' class="form-control" >
+						<input type="text" name='father_name' value="{{old('father_name')}}" class="form-control" >
 					</div>
 					</div>
 					<div class="col-md-6">
 					<div class="form-group">
 						<label for="mother_name">Mother's Name</label>
-						<input type="text" name='mother_name' class="form-control">
+						<input type="text" name='mother_name' value="{{old('mother_name')}}" class="form-control">
 					</div>
 					</div>
 					</div>
@@ -112,7 +112,7 @@ Employee Registration
 					<div class="col-md-3">
 					<div class="form-group">
 						<label for="disability">Disability</label>
-						<input type="text" name='disability' class="form-control">
+						<input type="text" name='disability' value="{{old('disability')}}" class="form-control">
 					</div>
 					</div>
 					<div class="col-md-3">
@@ -139,13 +139,13 @@ Employee Registration
 					<div class="col-md-3">
 					<div class="form-group">
 						<label for="county">Nationality</label>
-						<input type="text" id="nationality" name='county' class="form-control"required>
+						<input type="text" id="nationality" name='county' value="{{old('nationality')}}" class="form-control"required>
 					</div>
 					</div>
 					<div class="col-md-3">
 					<div class="form-group">
 						<label for="country">Formal Nationality</label>
-						<input type="text" name='country' class="form-control">
+						<input type="text" name='country' value="{{old('country')}}" class="form-control">
 					</div>
 					</div>
 					
@@ -161,7 +161,7 @@ Employee Registration
 						<div class="col-md-4">
 						<div class="form-group">
 							<label for="visa">Work Visa</label>
-							<input type="text" name='visa' class="form-control"required>
+							<input type="text" name='visa' value="{{old('visa')}}" class="form-control"required>
 						</div>
 						</div>
 						<div class="col-md-4">
@@ -173,7 +173,7 @@ Employee Registration
 						<div class="col-md-4">
 							<div class="form-group">
 								<label for="work_permit">Work Permit:</label>
-								<input type="file" name="work_permit" class="form-control">
+								<input type="file" name="work_permit" value="{{old('work_permit')}}" class="form-control">
 							</div>
 						</div>
 					</div>
@@ -196,19 +196,19 @@ Employee Registration
 					<div class="col-md-4">
 					<div class="form-group">
 						<label for="permanent_address">Permanent Home Address</label>
-						<span id="permanent_target"><input type="text" id="permanent" name='permanent_address' class="form-control"required></span>
+						<span id="permanent_target"><input type="text" id="permanent" name='permanent_address' value="{{old('permanent_address')}}" class="form-control"required></span>
 					</div>
 					</div>
 					<div class="col-md-4">
 					<div class="form-group">
 						<label for="temporary_address">Temporary Home Address</label>
-						<span id="temporary_target"><input type="text" id="temporary" name='temporary_address' class="form-control"required></span>
+						<span id="temporary_target"><input type="text" id="temporary" name='temporary_address' value="{{old('temporary_address')}}" class="form-control"required></span>
 					</div>
 					</div>
 					<div class="col-md-4">
 					<div class="form-group">
 						<label for="home_phone">Home Phone</label>
-						<input type="text" name='home_phone' class="form-control">
+						<input type="text" name='home_phone' value="{{old('home_phone')}}" class="form-control">
 					</div>
 					</div>
 				</div>
@@ -216,13 +216,13 @@ Employee Registration
 					<div class="col-md-6">
 					<div class="form-group">
 						<label for="mobile_phone">Mobile Phone</label>
-						<input type="text" name='mobile_phone' class="form-control"required>
+						<input type="text" name='mobile_phone' value="{{old('mobile_phone')}}" class="form-control"required>
 					</div>
 					</div>
 					<div class="col-md-6">
 					<div class="form-group">
 						<label for="email">Email</label>
-						<input type="text" name='email' class="form-control"required>
+						<input type="text" name='email' value="{{old('email')}}" class="form-control"required>
 					</div>
 					</div>
 				</div>
@@ -244,19 +244,19 @@ Employee Registration
 					<div class="col-md-4">
 					<div class="form-group">
 						<label for="qualification">Quaification</label>
-						<input type="text" name='qualification' class="form-control">
+						<input type="text" name='qualification' value="{{old('qualification')}}" class="form-control">
 					</div>
 					</div>
 					<div class="col-md-4">
 					<div class="form-group">
 						<label for="experience">Experience</label>
-						<input type="text" name='experience' class="form-control">
+						<input type="text" name='experience' value="{{old('experience')}}" class="form-control">
 					</div>
 					</div>
 					<div class="col-md-4">
 					<div class="form-group">
 						<label for="exp_in_dept">Experience In: Department</label>
-						<input type="text" name='exp_in_dept' class="form-control">
+						<input type="text" name='exp_in_dept' value="{{old('exp_in_dept')}}" class="form-control">
 					</div>
 					</div>
 				</div>
@@ -264,7 +264,7 @@ Employee Registration
 					<div class="col-md-4">
 					<div class="form-group">
 						<label for="hired_for_dep">Hired For: Department</label>
-						<input type="text" name='hired_for_dep' class="form-control">
+						<input type="text" name='hired_for_dep' value="{{old('hired_for_dep')}}" class="form-control">
 					</div>
 					</div>
 					<div class="col-md-4">
@@ -276,7 +276,7 @@ Employee Registration
 					<div class="col-md-4">
 						<div class="form-group">
 							<label for="utility_bill">Utility Bill:</label>
-							<input type="file" name="utility_bill" class="form-control">
+							<input type="file" name="utility_bill" value="{{old('utility_bill')}}" class="form-control">
 						</div>
 					</div>
 					
@@ -295,7 +295,7 @@ Employee Registration
 					<div class="col-md-4">
 					<div class="form-group">
 						<label for="rate">Rate Contract</label>
-						<input type="text" name='rate' class="form-control"required>
+						<input type="text" name='rate' value="{{old('rate')}}" class="form-control"required>
 					</div>
 					</div>
 					<div class="col-md-4">
@@ -337,13 +337,13 @@ Employee Registration
 					<div class="col-md-6">
 					<div class="form-group">
 						<label for="emer_contact_name">Contact Name</label>
-						<input type="text" name='emer_contact_name' class="form-control">
+						<input type="text" name='emer_contact_name' value="{{old('emer_contact_name')}}" class="form-control">
 					</div>
 					</div>
 					<div class="col-md-6">
 					<div class="form-group">
 						<label for="emer_contact_address">Contact Address</label>
-						<input type="text" name='emer_contact_address' class="form-control">
+						<input type="text" name='emer_contact_address' value="{{old('emer_contact_address')}}" class="form-control">
 					</div>
 					</div>
 				</div>
@@ -351,19 +351,19 @@ Employee Registration
 					<div class="col-md-4">
 					<div class="form-group">
 						<label for="emer_contact_phone">Contact Phone No.</label>
-						<input type="text" name='emer_contact_phone' class="form-control">
+						<input type="text" name='emer_contact_phone' value="{{old('emer_contact_phone')}}" class="form-control">
 					</div>
 					</div>
 					<div class="col-md-4">
 					<div class="form-group">
 						<label for="emer_contact_email">Contact Email</label>
-						<input type="text" name='emer_contact_email' class="form-control">
+						<input type="text" name='emer_contact_email' value="{{old('emer_contact_email')}}" class="form-control">
 					</div>
 					</div>
 					<div class="col-md-4">
 					<div class="form-group">
 						<label for="emer_contact_ralation">Relation</label>
-						<input type="text" name='emer_contact_ralation' class="form-control">
+						<input type="text" name='emer_contact_ralation' value="{{old('emer_contact_ralation')}}" class="form-control">
 					</div>
 					</div>
 				</div>
@@ -385,13 +385,13 @@ Employee Registration
 					<div class="col-md-6">
 					<div class="form-group">
 						<label for="sort_code">SORT Code</label>
-						<input type="text" name='sort_code' class="form-control"required>
+						<input type="text" name='sort_code' value="{{old('sort_code')}}" class="form-control"required>
 					</div>
 					</div>
 					<div class="col-md-6">
 					<div class="form-group">
 						<label for="account_no">Account No</label>
-						<input type="text" name='account_no' class="form-control"required>
+						<input type="text" name='account_no' value="{{old('account_no')}}" class="form-control"required>
 					</div>
 					</div>
 				</div>
@@ -399,13 +399,13 @@ Employee Registration
 					<div class="col-md-6">
 					<div class="form-group">
 						<label for="bank_name">Bank Name</label>
-						<input type="text" name='bank_name' class="form-control"required>
+						<input type="text" name='bank_name' value="{{old('bank_name')}}" class="form-control"required>
 					</div>
 					</div>
 					<div class="col-md-6">
 					<div class="form-group">
 						<label for="bank_address">Bank Address</label>
-						<input type="text" name='bank_address' class="form-control"required>
+						<input type="text" name='bank_address' value="{{old('bank_address')}}" class="form-control"required>
 					</div>
 					</div>
 				</div>
@@ -419,13 +419,13 @@ Employee Registration
 					<div class="col-md-6">
 					<div class="form-group">
 						<label for="tax_ref_no">Tax Ref No.</label>
-						<input type="text" name='tax_ref_no' class="form-control">
+						<input type="text" name='tax_ref_no' value="{{old('tax_ref_no')}}" class="form-control">
 					</div>
 					</div>
 					<div class="col-md-6">
 					<div class="form-group">
 						<label for="national_insurance_no">National Insurance No.</label>
-						<input type="text" name='national_insurance_no' class="form-control"required>
+						<input type="text" name='national_insurance_no' value="{{old('national_insurance_no')}}" class="form-control"required>
 					</div>
 					</div>
 				</div>
@@ -453,7 +453,7 @@ Employee Registration
 		
 		$(document).ready(function(){
 	    $("#yespassport").click(function(){
-	    	var data = '<div class="box box-info"><div class="box-header with-border"><h3 class="box-title"><strong>{{"Passport Information"}}</strong></h3><div class="box-tools pull-right"><button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button><button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button></div></div><div class="box-body"><div class="row"><div class="col-md-6"><div class="form-group"><label for="passport_no">Passport Number</label><input type="text" name="passport_no" required class="form-control"></div></div><div class="col-md-6"><div class="form-group"><label for="passport_expiry_date">Passport Expire date</label><input type="date" name="passport_expiry_date" value="{{$date}}" required class="form-control"></div></div></div><div class="row"><div class="col-md-6"><div class="form-group"><label for="passport_place">Place of Issue</label><input type="text" name="passport_place" required class="form-control"></div></div><div class="col-md-6"><div class="form-group"><label for="passport_issue_date">Date Of Issue</label><input type="date" name="passport_issue_date" value="{{$date}}" required class="form-control"></div></div></div><div class="row"><div class="col-md-6"><div class="form-group"><label for="passport_front">Passport Front:</label><input type="file" name="passport_front" class="form-control"></div></div><div class="col-md-6"><div class="form-group"><label for="passport_back">Passport Back:</label><input type="file" name="passport_back" class="form-control"></div></div></div></div></div>';
+	    	var data = '<div class="box box-info"><div class="box-header with-border"><h3 class="box-title"><strong>{{"Passport Information"}}</strong></h3><div class="box-tools pull-right"><button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button><button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button></div></div><div class="box-body"><div class="row"><div class="col-md-6"><div class="form-group"><label for="passport_no">Passport Number</label><input type="text" name="passport_no" value="{{old("passport_no")}}" required class="form-control"></div></div><div class="col-md-6"><div class="form-group"><label for="passport_expiry_date">Passport Expire date</label><input type="date" name="passport_expiry_date" value="{{$date}}" required class="form-control"></div></div></div><div class="row"><div class="col-md-6"><div class="form-group"><label for="passport_place">Place of Issue</label><input type="text" name="passport_place" value="{{old("passport_place")}}" required class="form-control"></div></div><div class="col-md-6"><div class="form-group"><label for="passport_issue_date">Date Of Issue</label><input type="date" name="passport_issue_date" value="{{$date}}" required class="form-control"></div></div></div><div class="row"><div class="col-md-6"><div class="form-group"><label for="passport_front">Passport Front:</label><input type="file" name="passport_front" value="{{old("passport_front")}}" class="form-control"></div></div><div class="col-md-6"><div class="form-group"><label for="passport_back">Passport Back:</label><input type="file" name="passport_back" value="{{old("passport_back")}}" class="form-control"></div></div></div></div></div>';
 	        $("#passport").html(data);   
 	        });
 	    });
@@ -477,7 +477,7 @@ Employee Registration
 	    $("#permanent").on('keyup',function(){
 	    	var temp = this.value;
 	    	if (temp != '') {
-	    		var data = '<input type="text" id="temporary" name="temporary_address" class="form-control" >';
+	    		var data = '<input type="text" id="temporary" value="{{old("temporary_address")}}" name="temporary_address" class="form-control" >';
 	        	$("#temporary_target").html(data);   
 	    	}
 	        });
@@ -486,7 +486,7 @@ Employee Registration
 	    $("#temporary").on('keyup',function(){
 	    	var temp = this.value;
 	    	if (temp != '') {
-	    		var data = '<input type="text" id="permanent" name="permanent_address" class="form-control" >';
+	    		var data = '<input type="text" id="permanent" name="permanent_address" value="{{old("permanent_address")}}" class="form-control" >';
 	        	$("#permanent_target").html(data);   
 	    	}
 	        });
