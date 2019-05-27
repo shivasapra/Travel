159ec6bb0388164cbd181cdc6a32092684d529da
shivasapra <ashishsapra7@gmail.com>
2019-05-27 13:34:03 +0530

    Invoice Add fields

--- resources/views/invoice/create.blade.php
+++ resources/views/invoice/create.blade.php
@@ -71,6 +71,271 @@ Create Invoice
 			</div>
 		</div>
 		</div>
+
+
+		<div class="invoice" style="margin:0 0 30px 0;">
+			<div class="card">
+				<div class="card-header text-danger font-weight-bold">General PNR Info</div>
+				<div class="card-body p-0">
+					<div class="table-responsive">
+						<table class="table table-bordered m-0">
+							<thead>
+							<tr>
+								<th>Creation Date <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Universal PNR <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>PNR <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Agency PCC <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Airline Ref <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+							</tr>
+							</thead>
+							<tbody>
+							<tr>
+								<td>Friday, 10 May 2019</td>
+								<td>T4LQU5</td>
+								<td>8H6GQE</td>
+								<td>TVPORTDYNASKLTONWABAGENC</td>
+								<td>KCZFHZ</td>
+							</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+
+			<div class="card mt-3">
+				<div class="card-header text-danger font-weight-bold">
+					<div class="row">
+					<div class="col-sm-6">Passenger Details</div>
+					<div class="col-sm-6 text-right"><button type="button" onclick="myCreateFunction()" class="btn btn-primary">Add Passenger</button></div>
+					</div>
+				</div>
+				<div class="card-body p-0">
+					<div class="table-responsive">
+						<table id="myTable" class="table table-bordered m-0" style="margin-top:10px;">
+							<thead>
+							<tr>
+								<th>Pax Id <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Pax Type* <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>First Name <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Last Name <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Date of Birth* <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>&nbsp;</th>
+								<th>Fare Cost <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Fare Sell <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Tax Cost <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Tax Sell <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Total Sell <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>&nbsp;</th>
+							</tr>
+							</thead>
+							<tbody>
+							<tr>
+								<td>1</td>
+								<td>
+									<select class="form-control select-custom custom-select">
+										<option value="Adult">Adult</option>
+										<option value="Youth">Youth</option>
+										<option value="Child">Child</option>
+										<option value="Infant">Infant</option>
+									</select>
+								</td>
+								<td><input type="text" value="Harnam" class="form-control"></td>
+								<td><input type="text" value="Singh" class="form-control"></td>
+								<td><input type="text" placeholder="dd/mm/yyyy" value="" class="form-control"></td>
+								<td>Segment-1</td>
+								<td><input type="text" value="0" class="form-control" style="width:60px;"></td>
+								<td><input type="text" value="0" class="form-control" style="width:60px;"></td>
+								<td><input type="text" value="0" class="form-control" style="width:60px;"></td>
+								<td><input type="text" value="0" class="form-control" style="width:60px;"></td>
+								<td><input type="text" value="0" class="form-control" style="width:60px;"></td>
+								<td>&nbsp;</td>
+							</tr>
+							<tr>
+								<td colspan="5"></td>
+								<td>Segment-2</td>
+								<td><input type="text" value="0" class="form-control" style="width:60px;"></td>
+								<td><input type="text" value="0" class="form-control" style="width:60px;"></td>
+								<td><input type="text" value="0" class="form-control" style="width:60px;"></td>
+								<td><input type="text" value="0" class="form-control" style="width:60px;"></td>
+								<td><input type="text" value="0" class="form-control" style="width:60px;"></td>
+								<td>&nbsp;</td>
+							</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+			<div class="text-right mt-2">
+				<h6 class="text-danger font-weight-bold gbp-back"><span>Total GBP 806.05</span></h6>
+			</div>
+
+			<div class="card mt-3">
+				<div class="card-header text-danger font-weight-bold">Itinerary Details</div>
+				<div class="card-body p-0">
+					<div class="table-responsive">
+						<table class="table table-bordered m-0">
+							<thead>
+							<tr>
+								<th>Seg Id <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>From <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>To <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Carrier <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Flight <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Class <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Departure <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Time <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Arrival <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Time <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Status <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+							</tr>
+							</thead>
+							<tbody>
+							<tr>
+								<td>1</td>
+								<td><input type="text" value="LHR" class="form-control" disabled></td>
+								<td><input type="text" value="XYZ" class="form-control" disabled></td>
+								<td><input type="text" value="AC" class="form-control" disabled></td>
+								<td><input type="text" value="549" class="form-control" disabled></td>
+								<td><input type="text" value="S" class="form-control" disabled></td>
+								<td><input type="text" value="15052019" class="form-control" disabled></td>
+								<td><input type="text" value="14:10" class="form-control" disabled></td>
+								<td><input type="text" value="15052019" class="form-control" disabled></td>
+								<td><input type="text" value="16:55" class="form-control" disabled></td>
+								<td><input type="text" value="HK" class="form-control" disabled></td>
+							</tr>
+							<tr>
+								<td>1</td>
+								<td><input type="text" value="LHR" class="form-control" disabled></td>
+								<td><input type="text" value="XYZ" class="form-control" disabled></td>
+								<td><input type="text" value="AC" class="form-control" disabled></td>
+								<td><input type="text" value="549" class="form-control" disabled></td>
+								<td><input type="text" value="S" class="form-control" disabled></td>
+								<td><input type="text" value="15052019" class="form-control" disabled></td>
+								<td><input type="text" value="14:10" class="form-control" disabled></td>
+								<td><input type="text" value="15052019" class="form-control" disabled></td>
+								<td><input type="text" value="16:55" class="form-control" disabled></td>
+								<td><input type="text" value="HK" class="form-control" disabled></td>
+							</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+
+			<div class="card mt-3">
+				<div class="card-header text-danger font-weight-bold">Segment Related Cancellation/date Change Text</div>
+				<div class="card-body">
+					<h6 class="text-uppercase font-weight-bold">Note</h6>
+					<ul class="pl-4">
+						<li>Date changes before Departure (subject to original advance purchase conditions and same class of booking) :- not Permitted.</li>
+						<li>Date changes before Inbound Departure (subject to same class of booking) :- not Permitted.</li>
+						<li>Cancellation Fees (minimum 24 hours before departure) :- No Refunds.</li>
+					</ul>
+				</div>
+				<div class="card-body p-0">
+					<div class="table-responsive">
+						<table class="table table-bordered m-0">
+							<thead class="bg-light">
+							<tr>
+								<th>Seg Id <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Cancellation/Date Change Remarks <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>&nbsp;</th>
+							</tr>
+							</thead>
+							<tbody>
+							<tr>
+								<td>1</td>
+								<td><input type="text" value="LHR" class="form-control"></td>
+								<td>
+									<div class="custom-control custom-checkbox">
+										<input type="checkbox" class="custom-control-input" id="customCheck" name="example1">
+										<label class="custom-control-label" for="customCheck">Tick to hide from customer</label>
+									</div>
+								</td>
+							</tr>
+							<tr>
+								<td>2</td>
+								<td><input type="text" value="LHR" class="form-control"></td>
+								<td>
+									<div class="custom-control custom-checkbox">
+										<input type="checkbox" class="custom-control-input" id="customCheck" name="example1">
+										<label class="custom-control-label" for="customCheck">Tick to hide from customer</label>
+									</div>
+								</td>
+							</tr>
+							<tr>
+								<td>3</td>
+								<td><input type="text" value="LHR" class="form-control"></td>
+								<td>
+									<div class="custom-control custom-checkbox">
+										<input type="checkbox" class="custom-control-input" id="customCheck" name="example1">
+										<label class="custom-control-label" for="customCheck">Tick to hide from customer</label>
+									</div>
+								</td>
+							</tr>
+							<tr>
+								<td>4</td>
+								<td><input type="text" value="LHR" class="form-control"></td>
+								<td>
+									<div class="custom-control custom-checkbox">
+										<input type="checkbox" class="custom-control-input" id="customCheck" name="example1">
+										<label class="custom-control-label" for="customCheck">Tick to hide from customer</label>
+									</div>
+								</td>
+							</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+
+			<div class="card mt-3">
+				<div class="card-header text-danger font-weight-bold">Charge Details</div>
+				<div class="card-body p-0">
+					<div class="table-responsive">
+						<table class="table table-bordered m-0">
+							<thead>
+							<tr>
+								<th>Charge Type <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Segment Id <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Pax Id <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Pax Type <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Cost Amount <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Sell Amount <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Supplier <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+								<th>Remarks <a href="#" class="text-dark"><i class="fa fa-angle-down"></i></a></th>
+							</tr>
+							</thead>
+							<tbody>
+							<tr>
+								<td>ATOL</td>
+								<td>1</td>
+								<td>All</td>
+								<td>All</td>
+								<td>2.50</td>
+								<td>2.50</td>
+								<td>ATOL</td>
+								<td>N/A</td>
+							</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<div class="text-right mt-2">
+					<h6 class="text-danger font-weight-bold gbp-back"><span>Total GBP 2.05</span></h6>
+					<h6 class="text-danger font-weight-bold gbp-back"><span>Grand Total GBP 808.52</span></h6>
+				</div>
+				<div class="row">
+				<div class="col-sm-12">
+					<h4 class="text-danger font-weight-bold">Agent Remarks</h4>
+					<textarea class="form-control" name="" style="height:80px;"></textarea>
+					<a href="#" class="btn btn-success" style="margin-top:10px;">Submit</a>
+				</div>
+				</div>
+			</div>
+		</div>
+
+
 		<div class="box box-primary">
 		<div class="box-body">
 				<div class="row">
@@ -262,9 +527,79 @@ Create Invoice
     	$("#address").html(append);   
     	});
     });
-
 		
    	</script>
+
+
+	<script>
+        function myCreateFunction() {
+            var table = document.getElementById("myTable");
+            var row = table.insertRow(3);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+            var cell4 = row.insertCell(3);
+            var cell5 = row.insertCell(4);
+            var cell6 = row.insertCell(5);
+            var cell7 = row.insertCell(6);
+            var cell8 = row.insertCell(7);
+            var cell9 = row.insertCell(8);
+            var cell10 = row.insertCell(9);
+            var cell11 = row.insertCell(10);
+            var cell12 = row.insertCell(11);
+
+            var row_second = table.insertRow(4);
+            var cell01 = row_second.insertCell(0);
+            var cell02 = row_second.insertCell(1);
+            var cell03 = row_second.insertCell(2);
+            var cell04 = row_second.insertCell(3);
+            var cell05 = row_second.insertCell(4);
+            var cell06 = row_second.insertCell(5);
+            var cell07 = row_second.insertCell(6);
+            var cell08 = row_second.insertCell(7);
+            var cell09 = row_second.insertCell(8);
+            var cell010 = row_second.insertCell(9);
+            var cell011 = row_second.insertCell(10);
+            var cell012 = row_second.insertCell(11);
+
+            cell1.innerHTML = '2';
+            cell2.innerHTML = '<select class="form-control select-custom custom-select">\n' +
+                '              <option value="Adult">Adult</option>\n' +
+                '              <option value="Youth">Youth</option>\n' +
+                '              <option value="Child">Child</option>\n' +
+                '              <option value="Infant">Infant</option>\n' +
+                '            </select>';
+            cell3.innerHTML = '<input type="text" value="Harnam" class="form-control">';
+            cell4.innerHTML = '<input type="text" value="Singh" class="form-control">';
+            cell5.innerHTML = '<input type="text" placeholder="dd/mm/yyyy" value="" class="form-control">';
+            cell6.innerHTML = 'Segment-1';
+            cell7.innerHTML = '<input type="text" value="0" class="form-control" style="width:60px;">';
+            cell8.innerHTML = '<input type="text" value="0" class="form-control" style="width:60px;">';
+            cell9.innerHTML = '<input type="text" value="0" class="form-control" style="width:60px;">';
+            cell10.innerHTML = '<input type="text" value="0" class="form-control" style="width:60px;">';
+            cell11.innerHTML = '<input type="text" value="0" class="form-control" style="width:60px;">';
+            cell12.innerHTML = '<a href="javascript:void(0)" class="fa fa-remove text-danger" onclick="myDeleteFunction()"></a>';
+
+            cell01.innerHTML = '&nbsp';
+            cell02.innerHTML = '&nbsp';
+            cell03.innerHTML = '&nbsp';
+            cell04.innerHTML = '&nbsp';
+            cell05.innerHTML = '&nbsp';
+            cell06.innerHTML = 'Segment-2';
+            cell07.innerHTML = '<input type="text" value="0" class="form-control" style="width:60px;">';
+            cell08.innerHTML = '<input type="text" value="0" class="form-control" style="width:60px;">';
+            cell09.innerHTML = '<input type="text" value="0" class="form-control" style="width:60px;">';
+            cell010.innerHTML = '<input type="text" value="0" class="form-control" style="width:60px;">';
+            cell011.innerHTML = '<input type="text" value="0" class="form-control" style="width:60px;">';
+            cell012.innerHTML = '&nbsp';
+        }
+
+        function myDeleteFunction() {
+            document.getElementById("myTable").deleteRow(4);
+            document.getElementById("myTable").deleteRow(3);
+        }
+
+	</script>
 @stop
 
 		
