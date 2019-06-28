be0bfd4cef15393b6fab8d81a38d9238e9797196
shivasapra <ashishsapra7@gmail.com>
2019-06-28 15:48:05 +0530

    Client Editing Section Updated

--- app/Http/Controllers/clientController.php
+++ app/Http/Controllers/clientController.php
@@ -269,32 +269,92 @@ class clientController extends Controller
         $client->phone = $request->phone;
         $client->credit_limit = $request->credit_limit;
         $client->client_type = $request->client_type;
-        if ($request->passport_no != null ) {
-            $client->passport_no = $request->passport_no;
-            $client->passport_expiry_date = $request->passport_expiry_date;
-            $client->passport_issue_date = $request->passport_issue_date;
-            $client->passport_place = $request->passport_place;
-            if($request->hasFile('passport_front'))
-                {
-                $passport_front = $request->passport_front;
-                $passport_front_new_name = time().$passport_front->getClientOriginalName();
-                $passport_front->move('uploads/passport',$passport_front_new_name);
-                $client->passport_front = 'uploads/passport/'.$passport_front_new_name;
+        if ($request->has('passport')) {
+            $client->passport = $request->passport;
+            if ($request->passport == 1 ) {
+                $client->passport_no = $request->passport_no;
+                $client->passport_expiry_date = $request->passport_expiry_date;
+                $client->passport_issue_date = $request->passport_issue_date;
+                $client->passport_place = $request->passport_place;
+                if($request->hasFile('passport_front'))
+                    {
+                    $passport_front = $request->passport_front;
+                    $passport_front_new_name = time().$passport_front->getClientOriginalName();
+                    $passport_front->move('uploads/passport',$passport_front_new_name);
+                    $client->passport_front = 'uploads/passport/'.$passport_front_new_name;
                 }
-            if($request->hasFile('passport_back'))
-                {
-                $passport_back = $request->passport_back;
-                $passport_back_new_name = time().$passport_back->getClientOriginalName();
-                $passport_back->move('uploads/passport',$passport_back_new_name);
-                $client->passport_back = 'uploads/passport/'.$passport_back_new_name;
+                if($request->hasFile('passport_back'))
+                    {
+                    $passport_back = $request->passport_back;
+                    $passport_back_new_name = time().$passport_back->getClientOriginalName();
+                    $passport_back->move('uploads/passport',$passport_back_new_name);
+                    $client->passport_back = 'uploads/passport/'.$passport_back_new_name;
                 }
-            if($request->hasFile('letter'))
-                {
-                $letter = $request->letter;
-                $letter_new_name = time().$letter->getClientOriginalName();
-                $letter->move('uploads/passport',$letter_new_name);
-                $client->letter = 'uploads/passport/'.$letter_new_name;
+                if($request->hasFile('letter'))
+                    {
+                    $letter = $request->letter;
+                    $letter_new_name = time().$letter->getClientOriginalName();
+                    $letter->move('uploads/passport',$letter_new_name);
+                    $client->letter = 'uploads/passport/'.$letter_new_name;
                 }
+
+                do {
+                    //generate a random string using Laravel's str_random helper
+                    $token = str_random();
+                } //check if the token already exists and if it does, try again
+                while (client::where('token', $token)->first());
+                $client->token = $token;
+                $client->save();
+                // send the email
+                $contactEmail = $client->email;
+                $data = array('token'=>$token,'name'=>$client->first_name.' '.$client->last_name);
+                Mail::send('emails.clientConfirmation', $data, function($message) use ($contactEmail)
+                {
+                    $message->to($contactEmail)->subject( 'Permission For Keeping Your Details' );
+                });
+            }
+        }
+        else{
+            if ($request->passport_no != null) {
+                $client->passport_no = $request->passport_no;
+                $client->passport_expiry_date = $request->passport_expiry_date;
+                $client->passport_issue_date = $request->passport_issue_date;
+                $client->passport_place = $request->passport_place;
+                if($request->hasFile('passport_front'))
+                    {
+                    $passport_front = $request->passport_front;
+                    $passport_front_new_name = time().$passport_front->getClientOriginalName();
+                    $passport_front->move('uploads/passport',$passport_front_new_name);
+                    $client->passport_front = 'uploads/passport/'.$passport_front_new_name;
+                }
+                if($request->hasFile('passport_back'))
+                    {
+                    $passport_back = $request->passport_back;
+                    $passport_back_new_name = time().$passport_back->getClientOriginalName();
+                    $passport_back->move('uploads/passport',$passport_back_new_name);
+                    $client->passport_back = 'uploads/passport/'.$passport_back_new_name;
+                }
+                if($request->hasFile('letter'))
+                    {
+                    $letter = $request->letter;
+                    $letter_new_name = time().$letter->getClientOriginalName();
+                    $letter->move('uploads/passport',$letter_new_name);
+                    $client->letter = 'uploads/passport/'.$letter_new_name;
+                }
+            }
+        }
+
+        if ($request->has('permanent')) {
+            $client->permanent = $request->permanent;
+            if ($request->permanent == 1 ){
+                $client->currency = $request->currency;
+                $client->credit_limit = $request->credit_limit;
+            }
+        }
+        else{
+            if($request->credit_limit != null){
+                $client->credit_limit = $request->credit_limit;
+            }
         }
         $client->save();
         if($request->member_name){
--- resources/views/clients/edit.blade.php
+++ resources/views/clients/edit.blade.php
@@ -201,63 +201,34 @@ Edit Client
 						</div>
 					</div>
 				</div>
+				@else
+				<div class="row">
+					<div class="col-md-6">
+						<div class="form-group">
+							<label for="passport">Do you have passport</label>
+							<input type="radio" name='passport' required id="yespassport" value=1>Yes
+							<input type="radio" name='passport' required id="nopassport" checked value=0>No
+						</div>
+					</div>
+				</div>
+				<div id="passport"></div>
+				@endif
+				@if($client->permanent == 0)
+				<div class="row">
+					<div class="col-md-6">
+						<div class="form-group">
+							<label for="passport">Do you want to make this client permanent</label>
+							<input type="radio" name='permanent' required id="yespermanent" value=1>Yes
+							<input type="radio" name='permanent' required id="nopermanent" checked value=0>No
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div id="permanent"></div>
+					</div>
+				</div>
 				@endif
 				@if($client->family->count() > 0)
 				<?php $i=1?>
-				{{-- <div class="text-center"><h3>Family Members</h3></div><hr>
-				@foreach($client->family as $fam)
-				<div class="hatao">
-					<div class="row">
-						<div class="col-md-3">
-							<div class="form-group">
-								<label for="member_name[]"><strong>{{$i++}}</strong>. Member Name</label>
-								<input type="text" name="member_name[]" value="{{$fam->member_name}}" required class="form-control">
-							</div>
-						</div>
-						<div class="col-md-3">
-							<div class="form-group">
-								<label for="member_DOB[]">Member DOB</label>
-								<input type="date" name="member_DOB[]" value="{{$fam->member_DOB}}" required class="form-control">
-							</div>
-						</div>
-						<div class="col-md-3">
-							<div class="form-group">
-								<label for="member_passport_no[]">Member Passport NO.</label>
-								<input type="text" name="member_passport_no[]" value="{{$fam->member_passport_no}}" required class="form-control">
-							</div>
-						</div>
-						<div class="col-md-3">
-								<div class="form-group">
-									<label for="member_passport_place">Place of Issue</label>
-									<input type="text" name="member_passport_place[]" value="{{$fam->member_passport_place}}" required class="form-control">
-								</div>
-							</div>
-					</div>
-					<div class="row">
-						<div class="col-md-4">
-							<div class="form-group">
-								<label for="member_passport_place">Place of Issue</label>
-								<input type="text" name="member_passport_place[]" value="{{$fam->member_passport_place}}" required class="form-control">
-							</div>
-						</div>
-						<div class="col-md-4">
-							<div class="form-group">
-								<label for="member_passport_front">Passport Front:</label>
-								<input type="file" name="member_passport_front[]"  class="form-control" value="{{$fam->member_passport_front}}">
-							</div>
-						</div>
-						<div class="col-md-4">
-							<div class="form-group">
-							<label for="member_passport_back">Passport Back:</label>
-							<input type="file" name="member_passport_back[]"  class="form-control" value="{{$fam->member_passport_back}}">
-							</div>
-						</div>
-					</div>
-					<div align="right">
-						<input type="button" class="btn btn-danger btn-xs" value="Remove" onclick="SomeDeleteRowFunction(this);">	<br><br>
-					</div>
-				</div>
-				@endforeach --}}
 				@endif
 				<div id="family-member"></div>
 				<div align="right" style="margin-top: 5px">
@@ -279,7 +250,7 @@ Edit Client
 <script>
 	$(document).ready(function(){
     $("#add").click(function(){
-    		var append = '<div class="text-center"><h3>New Family Member</h3></div><hr><div class="hatao"><div class="row">	<div class="col-md-4">		<div class="form-group">			<label for="member_name[]">Member Name</label>			<input type="text" name="member_name[]" required class="form-control">		</div>	</div>	<div class="col-md-4">		<div class="form-group">			<label for="member_DOB[]">Member DOB</label>			<input type="date" name="member_DOB[]" value="{{$date}}" required class="form-control">		</div>	</div>	<div class="col-md-4">		<div class="form-group">			<label for="member_passport_no[]">Member Passport NO.</label>			<input type="text" name="member_passport_no[]" required class="form-control">		</div>	</div></div><div class="row"><div class="col-md-4"><div class="form-group"><label for="member_passport_place">Place of Issue</label><input type="text" name="member_passport_place[]" required class="form-control"></div></div><div class="col-md-4"><div class="form-group"><label for="member_passport_front">Passport Front:</label><input type="file" name="member_passport_front[]" class="form-control"></div></div><div class="col-md-4"><div class="form-group"><label for="member_passport_back">Passport Back:</label><input type="file" name="member_passport_back[]" class="form-control"></div></div></div><div align="right">						<input type="button" class="btn btn-danger btn-xs" value="Remove" onclick="SomeDeleteRowFunction(this);">	</div></div>';
+    		var append = '<div class="hatao"><div class="text-center"><h3>New Family Member</h3></div><hr><div class="row">	<div class="col-md-4">		<div class="form-group">			<label for="member_name[]">Member Name</label>			<input type="text" name="member_name[]" required class="form-control">		</div>	</div>	<div class="col-md-4">		<div class="form-group">			<label for="member_DOB[]">Member DOB</label>			<input type="date" name="member_DOB[]" value="{{$date}}" required class="form-control">		</div>	</div>	<div class="col-md-4">		<div class="form-group">			<label for="member_passport_no[]">Member Passport NO.</label>			<input type="text" name="member_passport_no[]" required class="form-control">		</div>	</div></div><div class="row"><div class="col-md-4"><div class="form-group"><label for="member_passport_place">Place of Issue</label><input type="text" name="member_passport_place[]" required class="form-control"></div></div><div class="col-md-4"><div class="form-group"><label for="member_passport_front">Passport Front:</label><input type="file" name="member_passport_front[]" class="form-control"></div></div><div class="col-md-4"><div class="form-group"><label for="member_passport_back">Passport Back:</label><input type="file" name="member_passport_back[]" class="form-control"></div></div></div><div align="right">						<input type="button" class="btn btn-danger btn-xs" value="Remove" onclick="SomeDeleteRowFunction(this);">	</div></div>';
         $("#family-member").append(append);
         });
     });
@@ -327,5 +298,31 @@ function SomeDeleteRowFunction(btndel) {
 						  reader.readAsDataURL(input.files[0]);
 					  }
 				  }
+
+				  $(document).ready(function(){
+	    $("#yespassport").click(function(){
+	    	var data = '<hr><div class="text-center"><h3>Passport Details</h3></div><hr><div class="row"><div class="col-md-6"><div class="form-group"><label for="passport_no">Passport Number</label><input type="text" name="passport_no" required class="form-control" maxlength="10"></div></div><div class="col-md-6"><div class="form-group"><label for="passport_expiry_date">Passport Expire date</label><input type="date" name="passport_expiry_date" required class="form-control"></div></div></div><div class="row"><div class="col-md-6"><div class="form-group"><label for="passport_place">Place of Issue</label><input type="text" name="passport_place" required class="form-control"></div></div><div class="col-md-6"><div class="form-group"><label for="passport_issue_date">Date Of Issue</label><input type="date" name="passport_issue_date" required class="form-control"></div></div></div><div class="row"><div class="col-md-4"><div class="form-group"><label for="passport_front">Passport Front:</label><input type="file" name="passport_front" class="form-control"></div></div><div class="col-md-4"><div class="form-group"><label for="passport_back">Passport Back:</label><input type="file" name="passport_back" class="form-control"></div></div><div class="col-md-4"><div class="form-group"><label for="passport_front">Letter:</label><input type="file" name="letter" class="form-control"></div></div></div><hr>';
+	        $("#passport").html(data);
+	        });
+	    });
+	    $(document).ready(function(){
+	    $("#nopassport").click(function(){
+	    	var data = '';
+	        $("#passport").html(data);
+	        });
+	    });
+
+	    $(document).ready(function(){
+	    $("#yespermanent").click(function(){
+	    	var data = '<div class="row"><div class="col-md-6"><div class="form-group"><label for="currency">Currency</label><select name="currency" class="form-control" id="currency"><option value="$">$</option><option value="&#163;" selected>&#163;</option></select></div></div><div class="col-md-6"><div class="form-group"><label for="credit_limit">Credit Limit</label><input type="text" name="credit_limit" required class="form-control"></div></div></div>';
+	        $("#permanent").html(data);
+	        });
+	    });
+	    $(document).ready(function(){
+	    $("#nopermanent").click(function(){
+	    	var data = '';
+	        $("#permanent").html(data);
+	        });
+	    });
 		  </script>
 @stop
