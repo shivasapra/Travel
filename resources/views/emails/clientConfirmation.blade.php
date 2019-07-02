52dc8617a7909cc62f2cac1a87e9b5663f79d8ff
shivasapra <ashishsapra7@gmail.com>
2019-07-02 13:14:15 +0530

    Passport Details Mailer Changes

--- resources/views/emails/clientConfirmation.blade.php
+++ resources/views/emails/clientConfirmation.blade.php
@@ -18,11 +18,9 @@
 <style>
 html {-webkit-text-size-adjust: none; -ms-text-size-adjust: none;}
 body{ font-size:16px;}
-	@media only screen and (min-device-width: 750px) {
-		.table750 {width: 850px !important;}
-	}
+
 	@media only screen and (max-device-width: 750px), only screen and (max-width: 750px){
-      table[class="table750"] {width: 100% !important;}
+
       .mob_b {width: 93% !important; max-width: 93% !important; min-width: 93% !important;}
       .mob_b1 {width: 100% !important; max-width: 100% !important; min-width: 100% !important;}
       .mob_left {text-align: left !important;}
@@ -36,19 +34,19 @@ body{ font-size:16px;}
    @media only screen and (max-device-width: 550px), only screen and (max-width: 550px){
       .mod_div {display: block !important;}
    }
-	.table750 {width: 850px;}
+
    .details-table th, .details-table td{padding:10px;}
 </style>
 </head>
 <body style="margin: 0; padding: 0;">
 
-<table cellpadding="0" cellspacing="0" border="0" width="100%" style="background: #f3f3f3; min-width: 550px; font-size: 1px; line-height: normal;">
+<table cellpadding="0" cellspacing="0" border="0" width="100%" style="background: #f3f3f3; font-size: 1px; line-height: normal;">
  	<tr>
    	<td align="center" valign="top">   			
    		<!--[if (gte mso 9)|(IE)]>
          <table border="0" cellspacing="0" cellpadding="0">
          <tr><td align="center" valign="top" width="750"><![endif]-->
-   		<table cellpadding="0" cellspacing="0" border="0" width="750" class="table750" style="width: 100%; max-width: 750px; min-width: 350px; background: #f3f3f3;">
+   		<table cellpadding="0" cellspacing="0" border="0" width="750" class="table750" style="width: 100%; background: #f3f3f3;">
    			<tr>
                <td class="mob_pad" width="25" style="width: 25px; max-width: 25px; min-width: 25px;">&nbsp;</td>
    				<td align="center" valign="top" style="background: #ffffff;">
@@ -73,7 +71,7 @@ body{ font-size:16px;}
                      </tr>
                   </table>
 
-                  <table cellpadding="0" cellspacing="0" border="0" width="88%" style="width: 88% !important; min-width: 88%; max-width: 88%;">
+                  <table cellpadding="0" cellspacing="0" border="0"  style="">
                      <tr>
                         <td align="left" valign="top">
                            <font face="'Source Sans Pro', sans-serif" color="#1a1a1a" style="font-size: 52px; line-height: 60px; font-weight: 300; letter-spacing: -1.5px;">
