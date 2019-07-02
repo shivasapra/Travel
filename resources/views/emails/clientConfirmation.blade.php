1a36d5063de01f28714a0ae69d18a03a653fbc51
shivasapra <ashishsapra7@gmail.com>
2019-07-02 13:21:09 +0530

    Passport Details Mailer Changes

--- resources/views/emails/clientConfirmation.blade.php
+++ resources/views/emails/clientConfirmation.blade.php
@@ -49,7 +49,7 @@ body{ font-size:16px;}
    		<table cellpadding="0" cellspacing="0" border="0" width="750" class="table750" style="width: 100%; background: #f3f3f3;">
    			<tr>
                <td class="mob_pad" width="25" style="width: 25px; max-width: 25px; min-width: 25px;">&nbsp;</td>
-   				<td align="center" valign="top" style="background: #ffffff;">
+   				<td align="center" valign="top" style="background: #ffffff;padding:25px;">
 
                   <table cellpadding="0" cellspacing="0" border="0" width="100%" style="width: 100% !important; min-width: 100%; max-width: 100%; background: #f3f3f3;">
                      <tr>
