a47ca51e640b2d5a04bca524c19ad7167492c019
shivasapra <ashishsapra7@gmail.com>
2019-07-03 15:54:41 +0530

    avatar fixes

--- resources/views/employee/show.blade.php
+++ resources/views/employee/show.blade.php
@@ -26,7 +26,7 @@ Employee
 			<li class="list-group-item"><a data-toggle="tab" href="#passport-information">Passport Information</a></li>																
 		</ul>
 		@if($employee->user)
-		<div class="image-div">
+		<div class="image-outer-div">
 			<br><img @if($employee->user->avatar) src="{{asset($employee->user->avatar)}}" @else src="{{asset('/images/user-placeholder.jpg')}}" @endif alt="passport back"  height="300px" width="300px" style="border-radius:10px">
 			<a href="{{asset($employee->passport_back)}}" download class="download-image-icon"><i class="fa fa-download" aria-hidden="true"></i></a>
 		</div>
