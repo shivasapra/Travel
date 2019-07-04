4ae09b405ff0032966108cb5fcd7b2fb08153551
shivasapra <ashishsapra7@gmail.com>
2019-07-04 11:30:50 +0530

    Leave Applications setup - model and controller

---
+++ database/migrations/2019_07_04_053714_create_leaves_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLeavesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leaves', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Leave Type')->nullable();
+            $table->Date('from')->nullable();
+            $table->Date('to')->nullable();
+            $table->string('no_of_days')->nullable();
+            $table->longText('comment')->nullable();
+            $table->string('pdf')->nullable();
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leaves');
+    }
+}
