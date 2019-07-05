7a1b41a72ce335d461b3b347c4804c708e916232
shivasapra <ashishsapra7@gmail.com>
2019-07-05 11:40:10 +0530

    Leave Application PDF FIxes

--- resources/views/leave/index.blade.php
+++ resources/views/leave/index.blade.php
@@ -195,7 +195,7 @@ function Fun2(temp){
                     
                     '<!-- Modal body -->'+
                     '<div class="modal-body">'+
-                        '<iframe src="{{route("show.pdf",["id"=>'+leave_id+'])}}" width="100%" height="500px"></iframe>'+
+                        '<iframe src="{{route("show.pdf",["id"=>'<script>console.log(leave_id);</script>'])}}" width="100%" height="500px"></iframe>'+
                     '</div>'+
                 '</div>'+
             '</div>'+
